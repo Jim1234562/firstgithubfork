291ad0687fe3dc55e7aba5b27a216ba54723986c
Jim1234562 <111119673+Jim1234562@users.noreply.github.com>
2024-08-13 01:46:27 +0200

    Update communicate.py

--- src/edge_tts/communicate.py
+++ src/edge_tts/communicate.py
@@ -171,12 +171,7 @@ def mkssml(tc: TTSConfig, escaped_text: Union[str, bytes]) -> str:
         escaped_text = escaped_text.decode("utf-8")
 
     # Return the SSML string.
-    return (<speak version="1.0" xmlns="https://www.w3.org/2001/10/synthesis" xml:lang="en-US">
-    <voice name="en-US-AvaNeural">
-        <phoneme alphabet="sapi" ph="iy eh n y uw eh s"> en-US </phoneme>
-    </voice>
-</speak>
-
+    return (
         "<speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='en-US'>"
         f"<voice name='en-US-AvaNeural'>"
         f"<phoneme alphabet='sapi' ph='iy eh n y uw eh s'> en-US </phoneme>"
